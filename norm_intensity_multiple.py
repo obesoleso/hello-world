3be5ad67681c81ed051ce20e8850b1d6c136a022
IMAGEO-MI\utente <guidoghik@gmail.com>
2016-10-12 14:19:14 +0200

    added time + creates a new folder where it puts the normed files

--- norm_intensity_multiple.py
+++ norm_intensity_multiple.py
@@ -5,16 +5,20 @@ che polyworks possa leggere l'intensita
 """
 
 import glob
+import time
+
 
 #folder = ("D:/Workspace_Files/user-data/test/")
 folder = ("D:/Workspace_Files/user-data/bbb/") # path del file
 ext = (".xyz") # estensione dei file
-files_in_folder = glob.glob(folder + "*" + ext) # elenca tutti i file all'interno della cartella con quella determinata estensione
+files_in_folder = glob.glob(folder + "normed/" + "*" + ext) # elenca tutti i file all'interno della cartella con quella determinata estensione
 print files_in_folder # printa tutti i file riconosciuti
 
 max_intensity = 0 # non toccare
 
 # CODICE
+
+start = time.clock()
 
 # il codice prima individua il fattore di normalizzazione della intensita all'interno di un gruppo di file e poi
 # lo applica a tutti quanti...
@@ -24,16 +28,14 @@ for items in files_in_folder:
     f = open(items, "r") # apre il file da cui leggere
     # g = open(items[:-4] + "_norm.xyz", "w") # crea un nuovo file con il suffisso "_norm"
     for line in f: # per ciascuna linea del file
-        print "max intensity is " + str(max_intensity)  # printa la intensita massima
+        #print "max intensity is " + str(max_intensity)  # printa la intensita massima
         parts = line.split() # splitta ciascuna riga del file in base agli spazi bianchi
-        print parts[-1] # printa l'ultima colonna
+        #print parts[-1] # printa l'ultima colonna
         if int(parts[-1]) > int(max_intensity): # if last column > max intensity
             max_intensity = parts[-1] # last column is the new max_intensity
 
-print "max intensity is " + max_intensity # printa la intensita massima
-
+#print "max intensity is " + max_intensity # printa la intensita massima
 fattore = 255 / float(max_intensity) # normalizza la max_intensity a 255 ... fattore e' float
-
 print "intensity factor is " + str(fattore) # printa il fattore di normalizzazione
 
 f.close() # chiude il file
@@ -49,3 +51,5 @@ for items in files_in_folder:
         # il \t e' cio' che viene inserito in mezzo ai singoli termini che erano una volta splittati ...  a questo si concatena la nuova intensita e poi si manda a capo con \n
     g.close() # chiude il file appena creato
     f.close() # chiude il file che e' stato letto
+
+print time.clock() - start
